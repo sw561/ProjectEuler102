8d1c0f530d8342ddc77ec6a39a1c81128ac17302
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2014-12-19 13:39:34 +0000

    Readme and setup script amended so others can use the code

--- setup.sh
+++ setup.sh
@@ -3,10 +3,3 @@
 ln -s ~/Documents/TestTemplate/test.h .
 ln -s ~/Documents/TestTemplate/test.cpp .
 ln -s ~/Documents/TestTemplate/program.py .
-
-cp ~/Documents/TestTemplate/debug.h .
-cp ~/Documents/TestTemplate/Makefile .
-cp ~/Documents/TestTemplate/script.py .
-cp ~/Documents/TestTemplate/test.py .
-cp ~/Documents/TestTemplate/main.cpp .
-cp ~/Documents/TestTemplate/.gitignore .
